5182c6a916838fe99c7f64f6fd1ab2e30d157323
Zinath22 <Zinath22@users.noreply.github.com>
2024-01-24 20:50:36 +0600

    get put code add

--- index.js
+++ index.js
@@ -23,6 +23,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+
+    app.get('/users', async (req, res) => {
+      const cursor = feedbackCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+  })
+  app.get('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await feedbackCollection.findOne(query)
+      res.send(result)
+  })
+  app.post('/users', async (req, res) => {
+      const users = req.body;
+      console.log(users);
+      const result = await feedbackCollection.insertOne(users);
+      res.send(result);
+  });
+  
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
